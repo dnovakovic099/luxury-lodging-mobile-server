024aa8c35e7c67471ba8bfdbe921f97b097b4bf9
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-04-29 19:42:22 +0545

    added auth middleware for fcm-token

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,12 +2,13 @@ import { AuthController } from "../controllers/AuthController";
 import { request, Router } from "express";
 import { validate } from "../helpers/validate";
 import { authSchema, fcmTokenSchema } from "../validation/authSchema";
+import authenticate from "../middlewares/authenticate";
 
 const router = Router();
 const authController = new AuthController();
 
 router.route('/signin').post(validate(authSchema), authController.signin);
 
-router.route('/fcm-token').post(validate(fcmTokenSchema), authController.saveFCMToken)
+router.route('/fcm-token').post(authenticate, validate(fcmTokenSchema), authController.saveFCMToken)
 
 export default router;
