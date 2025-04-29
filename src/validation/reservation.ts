593465611871b49d5b3ab5098f9d32a0ffee1ecc
Prasanna Ku. Baniya <prasannakb440@gmail.com>
2025-04-29 21:57:09 +0545

    updated validation for newReservationSchema

--- src/validation/reservation.ts
+++ src/validation/reservation.ts
@@ -1,10 +1,10 @@
 import Joi from "joi";
 
 export const newReservationSchema = Joi.object({
-    guestName: Joi.string().required(),
-    arrivalDate: Joi.string().required(),
-    departureDate: Joi.string().required(),
-    totalPrice: Joi.number().required(),
-    guestFirstName: Joi.string().required(),
-    listingName: Joi.string().required()
+    guestName: Joi.any().required(),
+    arrivalDate: Joi.any().required(),
+    departureDate: Joi.any().required(),
+    totalPrice: Joi.any().required(),
+    guestFirstName: Joi.any().required(),
+    listingName: Joi.any().required()
 });
